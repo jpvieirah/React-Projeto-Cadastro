627e1a29b823a7d0219833bfec790e01ae89f426
jpvieira1 <jp201101@gmail.com>
2022-03-25 09:21:38 -0300

    Integração JavaScript/JSX

--- fundamentos/src/pages/index.jsx
+++ fundamentos/src/pages/index.jsx
@@ -1,3 +1,8 @@
 export default function Home() {
-    return "Inicio"
+    return (
+        <div>
+            <h1>Fundamentos de Next.js & React</h1>
+            <h2>Vamos estudar esse Framework</h2>
+        </div>
+    )
 }
---
+++ fundamentos/src/pages/jsx.jsx
@@ -0,0 +1,16 @@
+export default function Jsx() {
+
+           
+    const titulo =  <h1>JSX é um conceito central.</h1>
+
+    function subtitulo() {
+        return <h2>{"Muito legal".toUpperCase() }</h2>
+    }
+
+    return (
+        <div>
+            {titulo}
+            {subtitulo()}
+        </div>
+    )
+}
