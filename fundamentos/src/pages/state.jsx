f32e8e9ff46747cb7995192096dfeb0a0be6494d
jpvieira1 <jp201101@gmail.com>
2022-03-25 12:51:43 -0300

    continua

---
+++ fundamentos/src/pages/state.jsx
@@ -0,0 +1,10 @@
+import Layout from "../components/Layout";
+
+export default function State() {
+    let number = 3
+    return (
+        <Layout title="Componente com Estado" >
+            <span></span>
+        </Layout>
+    )
+}
